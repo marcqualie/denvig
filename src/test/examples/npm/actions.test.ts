ad616382c86aa268f515dea24c7e821c4fc29f99
Marc Qualie <marc@marcqualie.com>
2025-08-07 03:51:08 +0100

    Use npm example instead of pnpm

--- src/test/examples/npm/actions.test.ts
+++ src/test/examples/npm/actions.test.ts
@@ -3,10 +3,10 @@ import { describe, it } from 'jsr:@std/testing/bdd'
 
 import { runTestCommand } from '../../utils/runTestCommand.ts'
 
-describe('examples / pnpm / actions', () => {
+describe('examples / npm / actions', () => {
   it('should run the package.json ls command', async () => {
     const result = await runTestCommand('denvig run ls', {
-      cwd: 'src/test/examples/pnpm',
+      cwd: 'src/test/examples/npm',
       env: {
         DENVIG_GLOBAL_CONFIG_PATH: `../../.denvig.global.test.yml`,
       },
@@ -20,13 +20,30 @@ describe('examples / pnpm / actions', () => {
 
   it('should run the custom denvig hello command', async () => {
     const result = await runTestCommand('denvig run hello', {
-      cwd: 'src/test/examples/pnpm',
+      cwd: 'src/test/examples/npm',
       env: {
         DENVIG_GLOBAL_CONFIG_PATH: `../../.denvig.global.test.yml`,
       },
     })
 
-    expect(result.stdout).toContain('Hello from pnpm example!')
+    console.log('$ denvig run hello')
+    console.log(`| code: ${result.code}`)
+    console.log(
+      '|- stdout:',
+      result.stdout
+        .split('\n')
+        .map((line) => `\n|    ${line}`)
+        .join(''),
+    )
+    console.log(
+      '|- stderr:\n',
+      result.stderr
+        .split('\n')
+        .map((line) => `\n|    ${line}`)
+        .join(''),
+    )
+
+    expect(result.stdout).toContain('Hello from npm example!')
     expect(result.stderr).toBe('')
     expect(result.code).toBe(0)
   })
